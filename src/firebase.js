7c6cf1b6e7a784b7cfff198ec6f0200af7fc01e6
MirzaShahzadSaleem <79744069+MirzaShahzadSaleem@users.noreply.github.com>
2022-11-01 15:48:13 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import firebase from 'firebase/compat/app';
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBo3i8h1VJGFTZeMCBusXLaGSuwooBAMKc",
-  authDomain: "blood-bank-62aac.firebaseapp.com",
-  projectId: "blood-bank-62aac",
-  storageBucket: "blood-bank-62aac.appspot.com",
-  messagingSenderId: "798863221314",
-  appId: "1:798863221314:web:ab32218581234983d5c3db",
-  measurementId: "G-6RDJ31QZ4Z"
+ 
 };
 
 
